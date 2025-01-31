e1eadda6d4e031c81612c540d1d29de26e0a4b2a
aniyagnik <ani789yagnik@gmail.com>
2025-01-31 17:33:35 +0530

    stats page-chart and calender added

--- app/(tabs)/statistics.tsx
+++ app/(tabs)/statistics.tsx
@@ -1,8 +1,19 @@
-import { View, StyleSheet, ScrollView, TouchableOpacity } from "react-native";
+import {
+  View,
+  StyleSheet,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 import React, { useState } from "react";
 import Entypo from "@expo/vector-icons/Entypo";
+import { BarChart,ContributionGraph } from "react-native-chart-kit";
+import { Dimensions } from "react-native";
+import { ContributionChartValue } from "react-native-chart-kit/dist/contribution-graph/ContributionGraph";
+import { RectProps } from "react-native-svg";
+
 export default function Statistics() {
-  const data = [
+  const tableData = [
     {
       date: "12/12/21",
       score: "2.6",
@@ -73,28 +84,77 @@ export default function Statistics() {
   const streakInfo = [
     {
       streakDays: 20,
-      streakText: "Best Streak",
-      StreakTime: "from 28/12/23 to 25/1/24",
+      streakText: "best",
+      StreakTime: "28/12/23 to 25/1/24",
     },
     {
       streakDays: 1,
-      streakText: "Current Streak",
-      StreakTime: "from 30/1/25",
+      streakText: "current",
+      StreakTime: "30/1/25",
     },
   ];
+  const data = {
+    labels: Array.from(Array(30)).map((e, i) => (i + 1).toString()),
+    datasets: [
+      {
+        data: Array.from(Array(30)).map(() => Math.floor(Math.random() * 10)),
+        color: (opacity = 1) => `rgba(34, 65, 244, ${opacity})`, // optional
+      },
+    ],
+  };
+  const commitsData = [
+    { date: "2025-01-02", count: 1 },
+    { date: "2025-01-03", count: 2 },
+    { date: "2025-01-04", count: 3 },
+    { date: "2025-01-05", count: 4 },
+    { date: "2025-01-06", count: 5 },
+    { date: "2025-01-30", count: 2 },
+    { date: "2025-01-31", count: 3 },
+    { date: "2025-03-01", count: 2 },
+    { date: "2025-02-02", count: 4 },
+    { date: "2025-02-05", count: 2 },
+    { date: "2025-02-30", count: 4 }
+  ];
+  const chartConfig = {
+    backgroundGradientFrom: "whitesmoke",
+    backgroundGradientFromOpacity: 1,
+    backgroundGradientTo: "whitesmoke",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 0.5) => `rgba(55, 0, 0, ${opacity})`,
+    strokeWidth: 1, // optional, default 3
+    barPercentage: 0.3,
+    barRadius: 10,
+
+    useShadowColorFromDataset: false, // optional
+  };
+  const handleCalenderPress = () => {
+    alert('calender cell clicked')
+  }
+  const handleToolTip:any={}
+  const screenWidth = Dimensions.get("window").width;
   return (
-    <ScrollView style={{ flex: 1 }}>
+    <ScrollView style={{ flex: 1,paddingVertical:10 }}>
       <View style={styles.statistics}>
+        <View style={styles.streakStat}>
+          <View style={styles.title}>Streak</View>
+          {streakInfo.map((item, index) => (
+            <View style={styles.streakBox} key={index}>
+              <Text style={styles.streakNumber}>{item.streakDays}</Text>
+              <Text style={styles.text}>{item.streakText}</Text>
+              <Text style={styles.text}>{item.StreakTime}</Text>
+            </View>
+          ))}
+        </View>
         <ScrollView>
           <View style={styles.table}>
             <View
               style={[styles.row, { backgroundColor: "gold", borderWidth: 0 }]}
             >
-              <View style={[styles.col, { minWidth: "20%" }]}>Date</View>
-              <View style={[styles.col, { minWidth: "20%" }]}>score</View>
-              <View style={[styles.col, { minWidth: "60%" }]}>remarks</View>
+              <Text style={[styles.col, { minWidth: "20%" }]}>Date</Text>
+              <Text style={[styles.col, { minWidth: "20%" }]}>score</Text>
+              <Text style={[styles.col, { minWidth: "60%" }]}>remarks</Text>
             </View>
-            {data.map((row, index) => (
+            {tableData.map((row, index) => (
               <View
                 style={[
                   styles.row,
@@ -107,10 +167,10 @@ export default function Statistics() {
                 ]}
                 key={index}
               >
-                <View style={[styles.col, { minWidth: "20%" }]}>
+                <Text style={[styles.col, { minWidth: "20%" }]}>
                   {row.date}
-                </View>
-                <View style={[styles.col, { minWidth: "20%" }]}>
+                </Text>
+                <Text style={[styles.col, { minWidth: "20%" }]}>
                   {row.score ? (
                     row.score
                   ) : (
@@ -120,34 +180,46 @@ export default function Statistics() {
                       color={row.isDone ? "green" : "gray"}
                     />
                   )}
-                </View>
-                <View
+                </Text>
+                <Text
                   style={[
                     styles.col,
                     {
                       minWidth: "60%",
                       maxWidth: "60%",
-                      alignItems: "flex-start",
                     },
                   ]}
                 >
                   {row.remarks}
-                </View>
-              </View>
-            ))}
-          </View>
-
-          <View style={styles.streakStat}>
-            <View style={styles.title}>Streak</View>
-            {streakInfo.map((item,index) => (
-              <View style={styles.streakBox} key={index}>
-                <View style={styles.streakNumber}>{item.streakDays}</View>
-                <View>{item.streakText}</View>
-                <View>{item.StreakTime}</View>
+                </Text>
               </View>
             ))}
           </View>
         </ScrollView>
+        <View style={styles.chart}>
+          <BarChart
+            data={data}
+            width={data.datasets[0].data.length * 20}
+            height={200}
+            yAxisLabel=""
+            yAxisSuffix=""
+            chartConfig={chartConfig}
+            showBarTops={false}
+            fromZero={true}
+          />
+        </View>
+        <View style={styles.calender}>
+        <ContributionGraph
+          values={commitsData}
+          endDate={new Date("2025-03-01")}
+          numDays={105}
+          width={screenWidth}
+          height={200}
+          chartConfig={chartConfig} 
+          tooltipDataAttrs={(value)=>handleToolTip}    
+          onDayPress={handleCalenderPress}
+        />
+        </View>
       </View>
     </ScrollView>
   );
@@ -157,24 +229,23 @@ const styles = StyleSheet.create({
   statistics: {
     display: "flex",
     flexDirection: "column",
-    height: 800,
+    height: 900,
     justifyContent: "space-around",
     padding: 20,
     gap: 20,
-    backgroundColor: "whitesmoke",
   },
   table: {
     minWidth: "100%",
     width: "auto",
-    maxHeight: "50%",
     overflowY: "scroll",
     justifyContent: "flex-start",
     alignContent: "stretch",
   },
   col: {
     padding: 10,
+    fontSize: 15,
+    display: "flex",
     justifyContent: "center",
-    alignItems: "center",
     wordWrap: "anywhere",
   },
   row: {
@@ -185,12 +256,11 @@ const styles = StyleSheet.create({
     alignContent: "stretch",
   },
   streakStat: {
-    marginTop: 10,
     backgroundColor: "rgb(220,220,220)",
     borderRadius: 10,
     display: "flex",
     flexDirection: "row",
-    height: 200,
+    height: 150,
     justifyContent: "center",
     flexWrap: "wrap",
   },
@@ -208,10 +278,24 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   streakNumber: {
-    borderWidth: 1,
     minHeight: "50%",
     justifyContent: "center",
     alignItems: "center",
+    fontSize: 45,
+    color: "crimson",
+    fontWeight: "bold",
+  },
+  text: {
+    fontSize: 15,
+    lineHeight: 20,
+  },
+  chart: {
+    maxWidth: "100%",
+    overflowX: "scroll",
+  },
+  calender: {
+    maxWidth: "100%",
+    overflowX: "scroll",
   },
 });
 
