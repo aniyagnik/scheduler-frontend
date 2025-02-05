e1f66e0bc7776874e90868726f8d55a2948040dc
aniyagnik <ani789yagnik@gmail.com>
2025-02-05 09:49:47 +0530

    changed create task ui

--- app/(tabs)/createTask.tsx
+++ app/(tabs)/createTask.tsx
@@ -4,6 +4,7 @@ import {
   Text,
   TouchableOpacity,
   StyleSheet,
+  Dimensions,
 } from "react-native";
 import React, { useState } from "react";
 import { Picker } from "@react-native-picker/picker";
@@ -17,8 +18,8 @@ const TaskDetails = () => {
     description: "",
     priority: "high",
     repetition: { type: "daily", value: "1" },
-    startFrom: new Date(),
-    isMeasurable: 0,
+    isMeasurable: 0, // 1 for measurable, 2 for yes|no
+    targetType: "", //atleast|atmost|exactly|any
     unit: "",
     target: "",
   });
@@ -26,127 +27,184 @@ const TaskDetails = () => {
   const router = useRouter();
 
   const submitForm = async () => {
+    //if(formData.isMeasurable==1)
     console.log(formData);
     const requestOptions = {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ ...formData }),
+      body: JSON.stringify({
+        ...formData,
+        isMeasurable: formData.isMeasurable == 1 ? true : false,
+      }),
     };
+    console.log("form data is " + requestOptions.body);
     try {
       await fetch("http://localhost:3000/api/v1/task", requestOptions).then(
         (response) => {
-          response.json().then((data) => {
-            alert(data.message);
-          });
+          response
+            .json()
+            .then((data) => alert(data.message))
+            .catch((error) => {
+              console.log(error);
+              alert("failed to create new task");
+            });
         }
       );
     } catch (error) {
       console.log(error);
     }
+    setFormData(() => ({
+      title: "",
+      description: "",
+      priority: "high",
+      repetition: { type: "daily", value: "1" },
+      isMeasurable: 0,
+      unit: "",
+      targetType: "",
+      target: "",
+    }));
     router.navigate("/dashboard");
   };
   return (
-    <View style={styles.createTask}>
-      <Text style={styles.heading}>Elaborate your habit </Text>
-      <ValidateTextInput
-        name="title"
-        placeholder="task name"
-        handleChange={(value: string) => {
-          setFormData((prevFormData) => ({
-            ...prevFormData,
-            title: value,
-          }));
-        }}
-      />
-      <TextInput
-        placeholder="description (optional)"
-        style={styles.textField}
-        onChangeText={(value: string) =>
-          setFormData((prevFormData) => ({
-            ...prevFormData,
-            description: value,
-          }))
-        }
-      />
-      <Picker
-        style={styles.picker}
-        selectedValue={formData.isMeasurable}
-        onValueChange={(value) => {
-          const val = value == 1 ? true : false;
-          return setFormData((prevFormData) => ({
-            ...prevFormData,
-            isMeasurable: value,
-          }));
-        }}
-      >
-        <Picker.Item label="Track progress by.." value={0} />
-        <Picker.Item label="measurable" value={1} />
-        <Picker.Item label="yes or no" value={2} />
-      </Picker>
-      <Picker
-        style={styles.picker}
-        selectedValue="daily"
-        onValueChange={(value) =>
-          setFormData((prevFormData) => ({
-            ...prevFormData,
-            repetition: { ...prevFormData.repetition, type: value },
-          }))
-        }
-      >
-        <Picker.Item value="daily" label="Daily" />
-        <Picker.Item value="weekly" label={"Weekly (on every Tuesday)"} />
-        <Picker.Item value="monthly" label="Monthly (on 2nd)" />
-      </Picker>
-      <View>
-        <label htmlFor="date">Start date</label>
-        <input
-          style={styles.dateInput}
-          type="date"
-          id="date"
-          aria-describedby="date-format"
-          min={new Date().toLocaleDateString()}
-          max="2031-01-01"
+    <View
+      style={[
+        styles.createTask,
+        { height: 0.8 * Dimensions.get("screen").height },
+      ]}
+    >
+      <Text style={styles.heading}>Create your habit </Text>
+      <View style={{ minHeight: 0.6 * Dimensions.get("screen").height }}>
+        <ValidateTextInput
+          name="title"
+          placeholder="task name"
+          handleChange={(value: string) => {
+            setFormData((prevFormData) => ({
+              ...prevFormData,
+              title: value,
+            }));
+          }}
         />
+        <View>
+          <Text style={{ color: "transparent" }}>|</Text>
+          <TextInput
+            placeholder="description (optional)"
+            style={styles.textField}
+            onChangeText={(value: string) =>
+              setFormData((prevFormData) => ({
+                ...prevFormData,
+                description: value,
+              }))
+            }
+          />
+        </View>
+        <View>
+          <Text style={{ color: "transparent" }}>|</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue={formData.isMeasurable}
+            onValueChange={(value) => {
+              return setFormData((prevFormData) => ({
+                ...prevFormData,
+                isMeasurable: value,
+              }));
+            }}
+          >
+            <Picker.Item label="Track progress by.." value={0} />
+            <Picker.Item label="measurable" value={1} />
+            <Picker.Item label="yes or no" value={2} />
+          </Picker>
+        </View>
+        <View>
+          <Text style={{ color: "transparent" }}>|</Text>
+          <Picker
+            style={styles.picker}
+            selectedValue="daily"
+            onValueChange={(value) =>
+              setFormData((prevFormData) => ({
+                ...prevFormData,
+                repetition: { ...prevFormData.repetition, type: value },
+              }))
+            }
+          >
+            <Picker.Item value="daily" label="Daily" />
+            <Picker.Item value="weekly" label={"Weekly (on every Tuesday)"} />
+            <Picker.Item value="monthly" label="Monthly (on 2nd)" />
+            <Picker.Item value="once" label="One time activity" />
+          </Picker>
+        </View>
+        {formData.isMeasurable == 1 ? (
+          <>
+            <Text style={{ color: "transparent" }}>|</Text>
+            <View style={styles.measurableContainer}>
+              <View>
+                <Text style={{ color: "transparent" }}>|</Text>
+                <Picker
+                  style={[
+                    styles.picker,
+                    { width: Dimensions.get("window").width / 4 },
+                  ]}
+                  selectedValue={formData.targetType}
+                  onValueChange={(value) => {
+                    return setFormData((prevFormData) => ({
+                      ...prevFormData,
+                      targetType: value,
+                    }));
+                  }}
+                >
+                  <Picker.Item label="at least" value="atleast" />
+                  <Picker.Item label="at most" value="atmost" />
+                  <Picker.Item label="any" value="any" />
+                  <Picker.Item label="exactly" value="exactly" />
+                </Picker>
+              </View>
+              <View style={{ width: Dimensions.get("window").width / 4 }}>
+                <ValidateTextInput
+                  name="unit"
+                  placeholder="unit e.g. hours"
+                  handleChange={(value: string) => {
+                    setFormData((prevFormData) => ({
+                      ...prevFormData,
+                      unit: value,
+                    }));
+                  }}
+                />
+              </View>
+              <View style={{ width: Dimensions.get("window").width / 4 }}>
+                <ValidateTextInput
+                  name="target"
+                  placeholder="target e.g. 8"
+                  handleChange={(value: string) => {
+                    setFormData((prevFormData) => ({
+                      ...prevFormData,
+                      target: value,
+                    }));
+                  }}
+                />
+              </View>
+            </View>
+          </>
+        ) : (
+          <></>
+        )}
       </View>
-      {formData.isMeasurable == 1 ? (
-        <>
-          <ValidateTextInput
-            name="unit"
-            placeholder="unit e.g. hours"
-            handleChange={(value: string) => {
-              setFormData((prevFormData) => ({
-                ...prevFormData,
-                unit: value,
-              }));
-            }}
+      <View style={styles.footer}>
+        <TouchableOpacity
+          activeOpacity={0.7}
+          style={[
+            styles.submitButton,
+            { width: Dimensions.get("window").width / 4 },
+          ]}
+          onPress={submitForm}
+        >
+          <MaterialIcons
+            style={{ display: "flex", justifyContent: "center" }}
+            name="add"
+            size={20}
+            color="black"
           />
-          <ValidateTextInput
-            name="target"
-            placeholder="target e.g. 8"
-            handleChange={(value: string) => {
-              setFormData((prevFormData) => ({
-                ...prevFormData,
-                target: value,
-              }));
-            }}
-          />
-        </>
-      ) : (
-        <></>
-      )}
-      <TouchableOpacity
-        activeOpacity={0.7}
-        style={styles.submitButton}
-        onPress={submitForm}
-      >
-        <MaterialIcons
-          style={{ display: "flex", justifyContent: "center" }}
-          name="add"
-          size={20}
-          color="black"
-        />
-        <Text style={styles.text}>Save</Text>
-      </TouchableOpacity>
+          <Text style={styles.text}>Save</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -158,12 +216,13 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "column",
     padding: 20,
-    justifyContent: "space-around",
+    justifyContent: "space-evenly",
     gap: 20,
   },
   heading: {
     color: "white",
-    paddingVertical: 5,
+    fontWeight: "bold",
+    paddingVertical: 10,
     backgroundColor: "blue",
     width: "100%",
     textAlign: "center",
@@ -184,11 +243,15 @@ const styles = StyleSheet.create({
     borderRadius: 2,
     borderBottomWidth: 1,
   },
+  measurableContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 10,
+  },
   submitButton: {
     justifyContent: "center",
     flexDirection: "row",
-    paddingHorizontal: 24,
-    paddingVertical: 5,
+    paddingVertical: 3,
     borderRadius: 5,
     boxShadow: "0 2px 2px gray",
     backgroundColor: "whitesmoke",
@@ -200,6 +263,13 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 15,
     color: "black",
+  },
+  footer: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "blue",
+    width: "100%",
+    borderBottomRightRadius: 40,
   },
   dateInput: {
     marginTop: 5,
